e23247ecb523909514ab5505cc22feb9807744df
teaaqueenn <the.teaa.queenn@gmail.com>
2025-01-02 11:03:07 +0800

    comments added

--- src/main/java/frc/robot/subsystems/LimelightOdom.java
+++ src/main/java/frc/robot/subsystems/LimelightOdom.java
@@ -16,6 +16,13 @@ public class LimelightOdom {
     private double llUpdateThreshold = 0.75;
     private boolean ignoreLimelight = false;
 
+    /**
+     * Constructor for LimelightOdom.
+     *
+     * @param initialModulePositions Initial positions of the swerve modules.
+     * @param initialHeading Initial heading of the robot.
+     * @param initialPose Initial pose of the robot.
+     */
     public LimelightOdom(SwerveModulePosition[] initialModulePositions, Rotation2d initialHeading, Pose2d initialPose) {
         // Initialize the odometry with the drivetrain data
         odometry = new SwerveDrivePoseEstimator(
@@ -26,6 +33,13 @@ public class LimelightOdom {
         );
     }
 
+    /**
+     * Updates the odometry with the latest module positions, current pose, and heading.
+     *
+     * @param modulePositions Current positions of the swerve modules.
+     * @param currentPose Current pose of the robot.
+     * @param currentHeading Current heading of the robot.
+     */
     public void update(SwerveModulePosition[] modulePositions, Pose2d currentPose, Rotation2d currentHeading) {
         // Update odometry with swerve drivetrain data
         odometry.update(currentHeading, modulePositions);
@@ -34,14 +48,29 @@ public class LimelightOdom {
         updateLimelightPose(currentPose);
     }
 
+    /**
+     * Retrieves the current estimated pose of the robot.
+     *
+     * @return The current estimated pose as a Pose2d object.
+     */
     public Pose2d getPose() {
         return odometry.getEstimatedPosition();
     }
 
+    /**
+     * Retrieves the current estimated heading of the robot.
+     *
+     * @return The current estimated heading as a Rotation2d object.
+     */
     public Rotation2d getHeading() {
         return getPose().getRotation();
     }
 
+    /**
+     * Updates the pose estimate using data from the Limelight.
+     *
+     * @param currentPose Current pose of the robot.
+     */
     private void updateLimelightPose(Pose2d currentPose) {
         // Set robot orientation for the Limelight camera
         LimelightHelpers.SetRobotOrientation(LIMELIGHT_NAME, currentPose.getRotation().getDegrees(), 35.0, 0, 0, 0, 0);
@@ -63,6 +92,13 @@ public class LimelightOdom {
         }
     }
 
+    /**
+     * Calculates the distance between two poses.
+     *
+     * @param curr Current pose.
+     * @param prev Previous pose.
+     * @return The Euclidean distance between the two poses.
+     */
     private double getLLposesDist(Pose2d curr, Pose2d prev) {
         return Math.sqrt(
             Math.pow((prev.getX() - curr.getX()), 2) +
@@ -70,11 +106,20 @@ public class LimelightOdom {
         );
     }
 
+    /**
+     * Checks if a pose is within valid field boundaries.
+     *
+     * @param pose The pose to validate.
+     * @return True if the pose is valid, false otherwise.
+     */
     private boolean validPose(Pose2d pose) {
         return pose.getX() > 0 && pose.getX() < 16 && pose.getY() > 0 && pose.getY() < 8;
     }
 
+    /**
+     * Updates the SmartDashboard with relevant Limelight data.
+     */
     public void updateDashboard() {
-       SmartDashboard.putNumber("Limelight Area", LimelightHelpers.getTA(LIMELIGHT_NAME));
+        SmartDashboard.putNumber("Limelight Area", LimelightHelpers.getTA(LIMELIGHT_NAME));
     }
 }
